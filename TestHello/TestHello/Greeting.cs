6be0f8dec88e2808f9ae2c48b99d031e989d7baf
suleman-altaf <altafs.air@gmail.com>
2019-09-23 19:51:23 +0500

    First commit

---
+++ TestHello/TestHello/Greeting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHello
+{
+    public class Greeting
+    {
+        public string SayHello()
+        {
+            return "Hello from Suleman, " + DateTime.Now;
+        }
+    }
+}
